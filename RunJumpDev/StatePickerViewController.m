343ca972359300d32ed2139aaf65c2a28e4b95db
Jun Huang <mightybeesun@gmail.com>
2014-07-02 19:01:52 -0400

    Set the selected state in picker

--- RunJumpDev/StatePickerViewController.m
+++ RunJumpDev/StatePickerViewController.m
@@ -31,12 +31,14 @@
     _currForm = [[FormData alloc] init];
     _currForm = [_currForm currFormData];
     
+
     self.DoneButton.layer.cornerRadius = 10;
     self.DoneButton.layer.borderColor = [UIColor colorWithRed:128.0/255.0 green:128.0/255.0 blue:128.0/255.0 alpha:1.0f].CGColor;
     self.DoneButton.layer.borderWidth = 2.0f;
     
     self.listOfStates = [[NSArray alloc] initWithObjects:@"AL",@"AK",@"AZ",@"AR",@"CA",@"CO",@"CT",@"DC",@"DE",@"FL",@"GA",@"HI",@"ID",@"IL",@"IN",@"IA",@"KS",@"KY",@"LA",@"ME",@"MD",@"MA",@"MI",@"MN"
                          ,@"MS",@"MO",@"MT",@"NE",@"NV",@"NH",@"NJ",@"NY",@"NC",@"ND",@"OH",@"OK",@"OR",@"PA",@"RI",@"SC",@"SD",@"TN",@"TX",@"UT",@"VT",@"VA",@"WA",@"WV",@"WI",@"WY", nil];
+    [self setDefaultRow];
 }
 
 - (void)didReceiveMemoryWarning
@@ -65,6 +67,23 @@
 {
     return [[self listOfStates] objectAtIndex:row];
 }
+
+-(void)setDefaultRow{
+    int iSetRow =  0;
+    int iLen = [self.listOfStates count];
+    
+    for ( int i = 0; i < iLen; ++i ){
+        NSString * currState = [self.listOfStates objectAtIndex:i];
+        if ( [currState isEqualToString:[_currForm state]] ){
+            iSetRow = i;
+            break;
+        }
+        
+    }
+    
+    [_StatePicker selectRow:iSetRow inComponent:0 animated:NO];
+}
+
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row   inComponent:(NSInteger)component
 {
     NSLog(@"Selected state: %@", self.listOfStates[row]);
